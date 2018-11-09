f270084fbe0b97eb894bf2dc31a12195b54523b2
OddE92 <36917006+OddE92@users.noreply.github.com>
2018-11-09 13:29:48 +0100

    Add files via upload

--- python/plot_RK4_approx.py
+++ python/plot_RK4_approx.py
@@ -12,7 +12,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.colors import Normalize
 
 trajectory = np.loadtxt('cpp_runge-kutta/RK4_approx_stepsizectrl.dat')                               # Reads the data from the RK4 approx
-trajectory2 = np.loadtxt('cpp_runge-kutta/RK4_approx_t.dat')
+#trajectory2 = np.loadtxt('cpp_runge-kutta/RK4_approx_t.dat')
 
 
 fig1 = plt.figure(1)
@@ -21,22 +21,22 @@ ax = fig1.gca(projection='3d')
 ax.plot3D(trajectory[:,0], trajectory[:,1], trajectory[:,2])
 
 ax.set_title('Trajectory RK4 step size control')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
+ax.set_xlabel('x [pc]')
+ax.set_ylabel('y [pc]')
+ax.set_zlabel('z [pc]')
 
 #fig2 = plt.figure(2)
 
 #plt.plot(trajectory[:,0], trajectory[:,1])
 
-fig2 = plt.figure(2)
-ax = fig2.gca(projection='3d')
+#fig2 = plt.figure(2)
+#ax = fig2.gca(projection='3d')
 
-ax.plot3D(trajectory2[:,0], trajectory2[:,1], trajectory2[:,2])
+#ax.plot3D(trajectory2[:,0], trajectory2[:,1], trajectory2[:,2])
 
-ax.set_title('Trajectory RK4')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
+#ax.set_title('Trajectory RK4')
+#ax.set_xlabel('x')
+#ax.set_ylabel('y')
+#ax.set_zlabel('z')
 
 plt.show()
